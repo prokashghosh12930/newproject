372824ec0d15ec356792abc2eb746a7a380d29eb
prokashghosh12930 <prokashghosh12930@gmail.com>
2025-02-20 18:00:58 +0530

    data changes.

---
+++ src/constants/dummy.ts
@@ -0,0 +1,102 @@
+import { Product } from "../types/ComponentProps";
+export const products: Product[] = [
+    {
+      id: 1,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+    {
+      id: 12,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+    {
+      id: 13,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+    {
+      id: 15,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+    {
+      id: 16,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+    {
+      id: 17,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+    {
+      id: 18,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+    {
+      id: 19,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+    {
+      id: 21,
+      title: "Essence Mascara Lash Princess",
+      description:
+        "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
+      price: 9.99,
+      rating: 4.94,
+      stock: 5,
+      brand: "Essence",
+      thumbnail: "https://via.placeholder.com/150",
+    },
+  ];
--- src/screens/app/ProductListScreen/index.tsx
+++ src/screens/app/ProductListScreen/index.tsx
@@ -5,112 +5,7 @@ import { height } from "../../../utils/Metrix";
 import { Product } from "../../../types/ComponentProps";
 import ProductCard from "../../../components/ProductCard";
 import Header from "../../../components/Header";
-
-
-const products: Product[] = [
-  {
-    id: 1,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  {
-    id: 12,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  {
-    id: 13,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  {
-    id: 15,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  {
-    id: 16,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  {
-    id: 17,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  {
-    id: 18,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  {
-    id: 19,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  {
-    id: 21,
-    title: "Essence Mascara Lash Princess",
-    description:
-      "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects.",
-    price: 9.99,
-    rating: 4.94,
-    stock: 5,
-    brand: "Essence",
-    thumbnail: "https://via.placeholder.com/150",
-  },
-  // Add more products as needed
-];
-
-
+import { products } from "../../../constants/dummy";
 
 const ProductListScreen: React.FC = () => {
   return (
@@ -122,8 +17,6 @@ const ProductListScreen: React.FC = () => {
         renderItem={({ item }) => <ProductCard product={item} />}
         style={{width:"100%"}}
       />
-
-      
     </SafeAreaView>
   );
 };
